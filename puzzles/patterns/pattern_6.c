87e68ad2b4ad20d9e81f20f55f81e64bcb6f984c
thekomali <250548503+thekomali@users.noreply.github.com>
2025-12-21 10:29:55 +0530

    puzzle 5

--- puzzles/patterns/pattern_6.c
+++ puzzles/patterns/pattern_6.c
@@ -3,11 +3,11 @@
 
 /*
 ======================
-1 
-2 2 
-3 3 3 
-4 4 4 4 
-5 5 5 5 5 
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1
 ======================
 */
 
@@ -15,10 +15,10 @@ int main(int argc, char *argv[]) {
   // get the row number from the command line
   int rows = atoi(argv[1]);
 
-  for (int r = 1; r <= rows; r++) {
-    for (int c = 1; c <= r; c++)
-      printf("%d ", r);
-    
+  for (int r = 0; r < rows; r++) {
+    for (int c = 1; c <= rows - r; c++)
+      printf("%d ", c);
+
     printf("\n");
   }
 

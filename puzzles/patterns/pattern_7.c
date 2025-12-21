a9d3f82a2d172e29e36ef1cd81a797acdbf67093
thekomali <250548503+thekomali@users.noreply.github.com>
2025-12-21 12:51:06 +0530

    puzzle 8

---
+++ puzzles/patterns/pattern_7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+======================
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+======================
+*/
+
+int main(int argc, char *argv[]) {
+  // get the row number from the command line
+  int rows = atoi(argv[1]);
+
+  for (int r = 0; r < rows; r++) {
+    // printing spaces
+    for (int s = rows - r - 1; s > 0; s--)
+      printf("  ");          // two spaces as we are adding once space after '*'
+
+    // printing symbols
+    for (int c = 0; c < (2 * r + 1); c++)
+      printf("* ");       
+
+    printf("\n");
+  }
+
+  return 0;
+}
---
+++ puzzles/patterns/pattern_8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+======================
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * *
+      * * *
+        *
+======================
+*/
+
+int main(int argc, char *argv[]) {
+  // get the row number from the command line
+  int rows = atoi(argv[1]);
+
+  // as it's inverted triangle, start from highest and decerment it
+  for (int r = rows - 1; r >= 0; r--) {
+    // printing spaces
+    for (int s = rows - r - 1; s > 0; s--)
+      printf("  ");          // two spaces as we are adding once space after '*'
+
+    // printing symbols
+    for (int c = 0; c < (2 * r + 1); c++)
+      printf("* ");       
+
+    printf("\n");
+  }
+
+  return 0;
+}
